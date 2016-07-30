c5491ecd6ebbdf5f6641fc0427fb998af33076ef
aleksander kutsenkov <ligalizer8@gmail.com>
2016-07-30 18:35:44 -0400

    params doesnt work

---
+++ mynewrails/app/controllers/articles_controller.rb
@@ -0,0 +1,26 @@
+class ArticlesController < ApplicationController
+  def index
+
+uri = URI("https://api.nytimes.com/svc/search/v2/articlesearch.json")
+http = Net::HTTP.new(uri.host, uri.port)
+http.use_ssl = true
+uri.query = URI.encode_www_form({
+  "api-key" => "9dbb30d38943454a81d7278ba9b333c3",
+  "q" => "usa",
+  "sort" => "newest"
+
+
+
+    # "q" : "$input.params().querystring"
+
+
+
+})
+request = Net::HTTP::Get.new(uri.request_uri)
+@result = JSON.parse(http.request(request).body)
+puts @result.inspect
+render json: @result
+
+
+  end
+end
